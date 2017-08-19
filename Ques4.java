1c14d67860a2a770715a9d56baf6f1453aaf1faa
Banwali-India-Pvt-Ltd <30862678+Banwali-India-Pvt-Ltd@users.noreply.github.com>
2017-08-19 17:52:07 +0530

    Add files via upload

---
+++ Ques4.java
@@ -0,0 +1,12 @@
+
+public class Ques4 {
+public static void main(String[] args){
+	
+	for(int i=1; i<=6; i++){
+		for(int j=1; j<i; j++){
+			System.out.print("*");
+		}
+		System.out.println(" ");
+	}
+}
+}
