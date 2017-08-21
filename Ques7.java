58d7ab09fe8ba992baf7a9116e061d854c751c3f
Banwali-India-Pvt-Ltd <30862678+Banwali-India-Pvt-Ltd@users.noreply.github.com>
2017-08-21 12:59:09 +0530

    Add files via upload

---
+++ Ques7.java
@@ -0,0 +1,11 @@
+
+public class Ques7 {
+public static void main(String[] args){
+	
+    for(int i=1;i<=9;i=i+2)
+    	for(int j=1;j<=i;j=j+2){
+    		System.out.print(j);
+    	}
+    System.out.println("");
+}
+}
