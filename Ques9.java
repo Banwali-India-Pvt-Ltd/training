b2cddbe9825322a364cf60d7f55bfcdb48f6b80c
Banwali-India-Pvt-Ltd <30862678+Banwali-India-Pvt-Ltd@users.noreply.github.com>
2017-08-21 13:02:29 +0530

    Add files via upload

---
+++ Ques9.java
@@ -0,0 +1,20 @@
+
+public class Ques9 {
+public static void main(String[] args){
+	
+	for(int i=2; i<=100; i++){
+		int flag=0;
+		for(int j=2; j<i; j++){
+			if(i%j==0){
+				flag=1;
+				break;
+			}
+		}
+		if(flag==0){
+			System.out.println(i);
+		}
+	 
+		 
+}
+}
+}
