67738e557e1a74a662577827d0f91e0bd800bff0
Banwali-India-Pvt-Ltd <30862678+Banwali-India-Pvt-Ltd@users.noreply.github.com>
2017-08-22 09:54:44 +0530

    Add files via upload

---
+++ SimpleIf.java
@@ -0,0 +1,12 @@
+
+public class SimpleIf {
+public static void main(String[] args){
+	int marks = 62;
+	if(marks>35){
+		System.out.println("Pass");
+	}
+	else{
+		System.out.println("Fail");
+	}
+}
+}
