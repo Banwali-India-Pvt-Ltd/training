f0f834f25bc0e326f8f42f9b566e30eaedf1cd3d
Banwali-India-Pvt-Ltd <30862678+Banwali-India-Pvt-Ltd@users.noreply.github.com>
2017-08-22 10:17:33 +0530

    Add files via upload

---
+++ IfElseIF.java
@@ -0,0 +1,18 @@
+
+public class IfElseIF {
+public static void main(String[] args){
+	int marks = 65;
+	if(marks>75){
+		System.out.println("Distinction");
+	}
+	else if(marks>60){
+		System.out.println("First Class");
+	}
+	else if(marks>50){
+		System.out.println("Second Class");
+	}
+	else{
+		System.out.println("Fail");
+	}
+}
+}
