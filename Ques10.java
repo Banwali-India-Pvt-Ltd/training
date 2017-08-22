1673f5999132bee00c40d58777410b23448c63f7
Banwali-India-Pvt-Ltd <30862678+Banwali-India-Pvt-Ltd@users.noreply.github.com>
2017-08-22 09:33:46 +0530

    Add files via upload

---
+++ Ques10.java
@@ -0,0 +1,16 @@
+
+public class Ques10 {
+public static void main(String[] args){
+	for(int i=1; i<=16; i=i+3){
+		for(int j=1; j<=10; j++){
+			System.out.println(i*j);
+		}
+		
+  
+	
+	
+	
+	 }
+	
+}
+}
