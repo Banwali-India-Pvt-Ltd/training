9b01538a856e152c5943fcc87136207b69546e26
Banwali-India-Pvt-Ltd <30862678+Banwali-India-Pvt-Ltd@users.noreply.github.com>
2017-08-22 11:42:10 +0530

    Add files via upload

---
+++ IFexample.java
@@ -0,0 +1,12 @@
+
+public class IFexample {
+public static void main(String[] args){
+	if(1<2){
+		System.out.println("1 is less than 2");
+	}
+	else 
+		System.out.println("2 is less than 1");
+	    System.out.println("Hello from ifexample");
+	
+}
+}
