071b2eae570cf0f68ae478d736e0524f74149652
Banwali-India-Pvt-Ltd <30862678+Banwali-India-Pvt-Ltd@users.noreply.github.com>
2017-08-22 10:02:10 +0530

    Add files via upload

---
+++ CondtionIF.java
@@ -0,0 +1,13 @@
+
+public class CondtionIF {
+public static void main(String[] args){
+	int a = 10;
+	int b = 5;
+	if(a>b){
+		System.out.println("a is greater than b");
+	}
+	else{
+		System.out.println("b is greater than a");
+	}
+}
+}
