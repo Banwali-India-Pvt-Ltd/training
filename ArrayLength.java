0ff25d4f3a741f9a6a4805e175b08b9a1558d562
Banwali-India-Pvt-Ltd <30862678+Banwali-India-Pvt-Ltd@users.noreply.github.com>
2017-08-25 16:39:22 +0530

    Add files via upload

---
+++ ArrayLength.java
@@ -0,0 +1,8 @@
+// The Program will get length of the given number
+
+public class ArrayLength {
+public static void main(String[] args){
+	int [] [] a = new int [3] [];
+	System.out.println(a.length);
+}
+}
