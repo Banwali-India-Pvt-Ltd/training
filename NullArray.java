b39d5bf90fc133f5ee5c9b0cfa027266f98db35a
Banwali-India-Pvt-Ltd <30862678+Banwali-India-Pvt-Ltd@users.noreply.github.com>
2017-08-25 16:51:49 +0530

    Add files via upload

---
+++ NullArray.java
@@ -0,0 +1,7 @@
+// The Program will give Null because of No value in 0 of a
+public class NullArray {
+public static void main(String[] args){
+	int [] [] a = new int [4] [];
+	System.out.println(a[0]);
+}
+}
