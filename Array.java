a01ceb3237cb73a1d6c9ec5187ea97709cebd37f
Banwali-India-Pvt-Ltd <30862678+Banwali-India-Pvt-Ltd@users.noreply.github.com>
2017-08-25 16:41:32 +0530

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,14 @@
+/* The program will print an error
+ * Or Null Pointer Exception
+ * Because there is not any value defined in the 0 of a
+ */
+
+public class Array {
+public static void main(String[] args){
+	int[] [] a = new int[4][];
+	System.out.println(a[0][0]);
+	
+	
+}
+     
+}
