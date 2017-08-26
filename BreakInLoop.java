daa0efdd28f55c1f604779652119241faec78d0d
Banwali-India-Pvt-Ltd <30862678+Banwali-India-Pvt-Ltd@users.noreply.github.com>
2017-08-26 16:54:19 +0530

    Add files via upload

---
+++ BreakInLoop.java
@@ -0,0 +1,21 @@
+/* The program of break statement
+ * break the number after defining a number
+ */
+// class name with first letter of capital
+public class BreakInLoop {
+	// used main method to write the program
+	public static void main(String[] args){
+		// implement for loop to print numbers
+		for(int i =1; i<=10; i++)
+		{
+			// condition to break the loop
+			if(i==5)
+			{
+				break;
+				
+			}
+			System.out.println(i);
+		}
+}
+
+}
---
+++ ContineInLoop.java
@@ -0,0 +1,20 @@
+/* The program is based on continue statement
+ * The actual work of continue statement to get skip the given number
+ */
+// The class with name of first capital letter
+public class ContineInLoop {
+	// main method to use program
+	public static void main(String[] args){
+		// implement for loop to print numbers
+	for(int i=1; i<=10; i++)
+	{
+		// implement if condition to skip three number
+		if(i==3)
+		{
+			continue;
+		}
+		System.out.println(i);
+	}
+
+}
+}
