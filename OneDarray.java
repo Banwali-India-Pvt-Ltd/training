f9ef89917ec7d4fe8e87311e9dfd390859527d41
Banwali-India-Pvt-Ltd <30862678+Banwali-India-Pvt-Ltd@users.noreply.github.com>
2017-08-27 21:43:27 +0530

    Add files via upload

---
+++ OneDarray.java
@@ -0,0 +1,18 @@
+// The program related to one D array
+public class OneDarray {
+	// main method to implement program
+	public static void main(String[] args)
+	{
+		int [] num = new int [5]; // Assigning the array 
+		
+		num[0] = 11;
+		num[1] = 22;
+		num[2] = 33;
+		num[3] = 44;
+		num[4] = 55;
+		
+		System.out.println(num[3]);
+		
+	}
+
+}
---
+++ SingleArray.java
@@ -0,0 +1,9 @@
+// This array stores lot's of value in single type
+public class SingleArray {
+// main method to implement program
+	public static void main(String[] args)
+	{
+		int [] num = { 11, 22, 33, 44, 55 }; // Store the values
+		System.out.println(num[3]);
+	}
+}
