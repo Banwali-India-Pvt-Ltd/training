8904f55582fe0397257797ac674828f3ae578b28
Banwali-India-Pvt-Ltd <30862678+Banwali-India-Pvt-Ltd@users.noreply.github.com>
2017-08-28 11:10:01 +0530

    Add files via upload

---
+++ IniArayWithAray.java
@@ -0,0 +1,16 @@
+// The program initialization an array with an array
+// class with first letter of capital
+public class IniArayWithAray {
+	// main method to implement program
+	public static void main(String[] args)
+	{
+		int [] num = { 11, 22, 33, 44, 55 };
+		// Store num into mark
+		int [] mark = num;
+	for(int i=0; i<5; i++)
+	{
+		System.out.println(mark[i]);
+	}
+	}
+
+}
