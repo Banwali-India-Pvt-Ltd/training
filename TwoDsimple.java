659092afc2307653d5782e7cc8fc3401fc9d079c
Banwali-India-Pvt-Ltd <30862678+Banwali-India-Pvt-Ltd@users.noreply.github.com>
2017-08-29 09:29:45 +0530

    Add files via upload

---
+++ TwoDsimple.java
@@ -0,0 +1,23 @@
+// The program related to simple two D array
+// class to create object
+public class TwoDsimple {
+	// main function to create program
+	public static void main(String[] args){
+		// Declaration and Initialization 
+		int [][] num = {{11, 22, 33, 44}, { 55, 66, 77, 88}, { 10, 20, 30, 40}};
+		// For Loop for rows
+		for(int i=0; i<3; i++)
+		{
+			// For Loop for columns
+			for(int j=0; j<4; j++)
+			{
+				System.out.println(num[i][j]);
+			}
+		}
+		
+		
+			
+		
+ 	}
+
+}
