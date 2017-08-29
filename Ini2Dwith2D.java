ba5a60c9f867d238b6b08f3ffbb1234b1fcef0b8
Banwali-India-Pvt-Ltd <30862678+Banwali-India-Pvt-Ltd@users.noreply.github.com>
2017-08-29 13:10:06 +0530

    Add files via upload

---
+++ Ini2Dwith2D.java
@@ -0,0 +1,13 @@
+// The program initialization an 2D array with an 2D Array
+// class is used to create object
+public class Ini2Dwith2D {
+	// main method to create program
+	public static void main(String[] args){
+		int [] [] num = {{11,22,33,44}, {55,66,77,88}, {10,20,30,40}};
+		int [][] mark = num;
+		
+		System.out.println(num[1][1]);
+		System.out.println(mark[1][2]);
+	}
+
+}
