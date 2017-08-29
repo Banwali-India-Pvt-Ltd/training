d0d8ca5d5774eff4c66afc0d415d855779f0781f
Banwali-India-Pvt-Ltd <30862678+Banwali-India-Pvt-Ltd@users.noreply.github.com>
2017-08-29 20:11:53 +0530

    Add files via upload

---
+++ 190.py
@@ -0,0 +1,11 @@
+def main():
+    print("Please enter five numbers: ")
+    # Allow the user to enter in the five values.
+    n1 = eval(input("Please enter number 1: "))
+    n2 = eval(input("Please enter number 2: "))
+    n3 = eval(input("Please enter number 3: "))
+    n4 = eval(input("Please enter number 4: "))
+    n5 = eval(input("Please enter number 5: "))
+    print("Numbers entered:", n1, n2, n3, n4, n5)
+    print("Average:", (n1 + n2 + n3 + n4 + n5)/5)
+main ()
