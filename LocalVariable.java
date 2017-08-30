119a949226f651f789871d4dfa289cef3232d675
Banwali-India-Pvt-Ltd <30862678+Banwali-India-Pvt-Ltd@users.noreply.github.com>
2017-08-30 14:54:29 +0530

    Add files via upload

---
+++ LocalVariable.java
@@ -0,0 +1,10 @@
+// The program create a local variable in side the main method
+// class is used to create object
+public class LocalVariable {
+	// main method to write program
+	public static void main(String[] args){
+		int roll = 2007; // local variable
+		System.out.println("The roll number is " + roll);
+	}
+
+}
