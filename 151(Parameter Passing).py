8755bccb2b86ffaa3e1c4a4534b0332dbbc8eb64
Banwali-India-Pvt-Ltd <30862678+Banwali-India-Pvt-Ltd@users.noreply.github.com>
2017-08-30 19:20:54 +0530

    Add files via upload

---
+++ 151(Parameter Passing).py
@@ -0,0 +1,5 @@
+name1 = "sukhpal"
+name2 = "choudhary"
+def display(par1,par2):
+    print(par1, par2)
+display(name1,name2)
---
+++ 151.2 (Record passing).py
@@ -0,0 +1,3 @@
+def record(name, id_no):
+    print name, id_no
+record(nassme = 'foo', id_no = 10)
