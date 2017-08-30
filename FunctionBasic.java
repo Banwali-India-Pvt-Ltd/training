f6e72547c98ecae3f7fda6ce70a3aa04d0a008db
Banwali-India-Pvt-Ltd <30862678+Banwali-India-Pvt-Ltd@users.noreply.github.com>
2017-08-30 09:30:39 +0530

    Add files via upload

---
+++ FunctionBasic.java
@@ -0,0 +1,11 @@
+
+public class FunctionBasic {
+public static void main(String[] args){
+	print("Hello");
+	print("How are you");
+	print("Bye");
+}
+   public static void print(String text){
+	   System.out.println(text);
+   }
+}
