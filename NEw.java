c9fed367927acd636b199047d1a8b3a4c0f3c024
Banwali-India-Pvt-Ltd <30862678+Banwali-India-Pvt-Ltd@users.noreply.github.com>
2017-08-30 17:45:22 +0530

    Add files via upload

---
+++ NEw.java
@@ -0,0 +1,26 @@
+// Import to sort data's
+import java.util.Arrays;
+public class NEw {
+    public static void main(String args[])
+    {   
+    	// To store the value
+    	int[] Nums = new int[6];
+    	Nums[0] = 10;
+    	Nums[1] = 14;
+    	Nums[2] = 36;
+    	Nums[3] = 27;
+    	Nums[4] = 43;
+    	Nums[5] = 18;
+    	// To sort data
+    	Arrays.sort(Nums);
+    	for(int i=0; i<Nums.length; i++){
+    		System.out.println(Nums[i]);
+    	}
+    	
+       
+       
+    }
+}
+
+
+
