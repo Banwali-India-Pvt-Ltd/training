43e0f781992e09de7fb44b2607137b1a32e0d656
Banwali-India-Pvt-Ltd <30862678+Banwali-India-Pvt-Ltd@users.noreply.github.com>
2017-08-30 09:57:51 +0530

    Add files via upload

---
+++ TableInFuntions.java
@@ -0,0 +1,17 @@
+// The program is for print table via functions
+// class to create object
+public class TableInFuntions {
+	// main method to create program
+public static void main(String[] args){
+	// define method to call it
+	printTimesTable(2);
+}
+// define a new function
+public static void printTimesTable(int param){
+	// implement loop to print table 
+	for(int i=1; i<=10; i++){
+		// finally display table
+		System.out.println(param * i);
+	}
+}
+}
