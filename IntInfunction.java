c65b413fdd1d21f03ec830e8537be80f81fa964c
Banwali-India-Pvt-Ltd <30862678+Banwali-India-Pvt-Ltd@users.noreply.github.com>
2017-08-31 14:35:37 +0530

    Add files via upload

---
+++ IntInfunction.java
@@ -0,0 +1,15 @@
+// The program to print int number
+// class to create method
+public class IntInfunction {
+	// main method to create or execute program
+public static void main(String[] args)
+{
+	// define number has to be print
+	print("10");
+	
+}
+// define new method by name of print
+public static void print(String number){
+	System.out.println(number);
+}
+}
