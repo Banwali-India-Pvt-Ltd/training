a7f1229c40b878f8fc4b79d03920009bc48daf01
Banwali-India-Pvt-Ltd <30862678+Banwali-India-Pvt-Ltd@users.noreply.github.com>
2017-08-31 10:38:31 +0530

    Add files via upload

---
+++ VoidExample.java
@@ -0,0 +1,23 @@
+// The program gives the Ranks 
+// class to create object
+public class VoidExample {
+	// main method to create program
+public static void main(String[] args){
+	// define new rank 
+	methodRank(255);
+}
+// define new method with name methodRank
+public static void methodRank(int points){
+	// implement the conditions to get the exact value
+	if(points>=266){
+		System.out.println("Rank1");
+	}
+	else if(points>=122){
+		System.out.println("Rank2");
+	}
+	else{
+		System.out.println("Rank3");
+	}
+	
+}
+}
