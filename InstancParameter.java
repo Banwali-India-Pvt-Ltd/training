f05d2833195841d99f0cfb193c64b638887c523a
Banwali-India-Pvt-Ltd <30862678+Banwali-India-Pvt-Ltd@users.noreply.github.com>
2017-08-31 17:06:29 +0530

    Add files via upload

---
+++ InstancParameter.java
@@ -0,0 +1,12 @@
+
+public class InstancParameter {
+public static void main(String[] args){
+	InstancParameter sc = new InstancParameter();
+	sc.disp(10,20);
+	
+}
+void disp(int a, int b){
+	int c = a+b;
+	System.out.println(c);
+}
+}
