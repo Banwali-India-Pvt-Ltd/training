9055608f8594232ecac7a00b6cb8a080f5dd9f5e
Banwali-India-Pvt-Ltd <30862678+Banwali-India-Pvt-Ltd@users.noreply.github.com>
2017-09-04 11:43:24 +0530

    Add files via upload

---
+++ IsUpperCase.java
@@ -0,0 +1,12 @@
+// The program checks weather the value is upper case or not
+// class to create method
+public class IsUpperCase 
+{
+	public static void main(String[] args)
+	{
+		System.out.println(Character.isUpperCase('C'));  // returns true
+		System.out.println(Character.isUpperCase('z'));  // returns false
+		System.out.println(Character.isUpperCase('a'));  // returns false
+	}
+
+}
