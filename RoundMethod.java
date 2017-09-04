516db92200253c53157946a190df55bac0990098
Banwali-India-Pvt-Ltd <30862678+Banwali-India-Pvt-Ltd@users.noreply.github.com>
2017-09-04 10:02:35 +0530

    Add files via upload

---
+++ RoundMethod.java
@@ -0,0 +1,18 @@
+// The program execute round numbers near the value
+
+// class to create method
+public class RoundMethod 
+{
+	// main method to create program
+	public static void main(String[] args)
+	{
+		double a = 10.575;
+		double b = 10.498;
+		int c = 11;
+		
+		System.out.println(Math.round(a)); // 11
+		System.out.println(Math.round(b)); // 10
+		System.out.println(Math.round(c));  // 11
+	}
+
+}
