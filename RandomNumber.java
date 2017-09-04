d43e247da598e3db730b7c7558daced927f7d162
Banwali-India-Pvt-Ltd <30862678+Banwali-India-Pvt-Ltd@users.noreply.github.com>
2017-09-04 10:34:31 +0530

    Add files via upload

---
+++ RandomNumber.java
@@ -0,0 +1,13 @@
+// The program generates random numbers after print
+// class to create method
+public class RandomNumber
+{
+	// main method to write program
+
+	public static void main(String[] args)
+{
+	
+	System.out.println(Math.random());
+	System.out.println(Math.random());
+}
+}
