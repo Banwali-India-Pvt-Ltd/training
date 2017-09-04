4af9abc17e0fd2e35d2fb1c71f051d21be42a407
Banwali-India-Pvt-Ltd <30862678+Banwali-India-Pvt-Ltd@users.noreply.github.com>
2017-09-04 11:55:12 +0530

    Add files via upload

---
+++ IsLowerCase.java
@@ -0,0 +1,12 @@
+// The program checks weather the value is lower case or not
+// class to create method
+public class IsLowerCase 
+{
+	public static void main(String[] args)
+	{
+		System.out.println(Character.isLowerCase('C'));  // return false
+		System.out.println(Character.isLowerCase('z'));  // returns true
+		System.out.println(Character.isLowerCase('A'));  // returns false
+	}
+
+}
