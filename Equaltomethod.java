0783fd6cc7942c29a5875a74f08c1ab9a247100b
Banwali-India-Pvt-Ltd <30862678+Banwali-India-Pvt-Ltd@users.noreply.github.com>
2017-09-04 09:51:18 +0530

    Add files via upload

---
+++ Equaltomethod.java
@@ -0,0 +1,15 @@
+// The program gets absolute value
+// class to create method
+public class Equaltomethod 
+{
+	// main method to create program
+	public static void main(String[] args)
+	{
+		// define values in the int and double
+		int a = -5;
+		double b = -10.234;
+		 
+		System.out.println(Math.abs(a));// 5
+		System.out.println(Math.abs(b));// 10.234
+    }
+}
