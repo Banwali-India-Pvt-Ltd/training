f73b30ff9cc69d34779736f367fc7fff34c59c9c
Banwali-India-Pvt-Ltd <30862678+Banwali-India-Pvt-Ltd@users.noreply.github.com>
2017-09-04 12:14:47 +0530

    Add files via upload

---
+++ ToUpperCase.java
@@ -0,0 +1,16 @@
+// The program converts value to Upper case
+// class to create method
+public class ToUpperCase 
+{
+	// main method to write the program
+	public static void main(String[] args)
+	{
+		System.out.println(Character.toUpperCase('a'));  // returns A
+		System.out.println(Character.toUpperCase('B'));  // returns B
+		System.out.println(Character.toUpperCase('A'));  // returns A
+		System.out.println(Character.toUpperCase('c'));  // returns C
+		
+		
+	}
+
+}
