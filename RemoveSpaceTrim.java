3db9cd41618114258cf8b0c9e1e876ac000a3427
Banwali-India-Pvt-Ltd <30862678+Banwali-India-Pvt-Ltd@users.noreply.github.com>
2017-09-05 10:34:14 +0530

    Add files via upload

---
+++ RemoveSpaceTrim.java
@@ -0,0 +1,16 @@
+// The program removes spaces from string
+// class to create method
+public class RemoveSpaceTrim
+{
+	// main method to create program
+public static void main(String[] args){
+	String str1 =" Banwali ";
+	String str2 ="  BANWALI";
+	String str3 ="banwali";
+	
+	System.out.println(str2);
+	System.out.println(str1.trim()); 
+	System.out.println(str2.trim());
+	System.out.println(str3.trim());
+}
+}
