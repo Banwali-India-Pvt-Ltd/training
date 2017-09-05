03e1500c4b512bd28c02131727841cb75ac68194
Banwali-India-Pvt-Ltd <30862678+Banwali-India-Pvt-Ltd@users.noreply.github.com>
2017-09-05 11:19:32 +0530

    Add files via upload

---
+++ EndsWithMethod.java
@@ -0,0 +1,16 @@
+// The program returns end value in the string
+// class to create method
+public class EndsWithMethod
+{
+	// main method to create program
+	public static void main(String[] args)
+	{
+		String str1 = "Banwali Company";
+		
+		System.out.println(str1.endsWith("Company"));
+		System.out.println(str1.endsWith("any"));
+		System.out.println(str1.endsWith("Banwali"));
+		System.out.println(str1.endsWith("MEthod"));
+	}
+
+}
