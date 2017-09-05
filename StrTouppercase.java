177b0220286e6f5abd8c31993e58957029b000ad
Banwali-India-Pvt-Ltd <30862678+Banwali-India-Pvt-Ltd@users.noreply.github.com>
2017-09-05 10:09:04 +0530

    Add files via upload

---
+++ StrTouppercase.java
@@ -0,0 +1,15 @@
+// The program convert the values in uppercase
+// class to create method
+public class StrTouppercase
+{
+	// main method to create program
+public static void main(String[] args){
+	String str1 ="selenium";
+	String str2 ="BANWALI";
+	String str3 ="Banwali";
+	 
+	System.out.println(str1.toUpperCase());
+	System.out.println(str2.toUpperCase());
+	System.out.println(str3.toUpperCase());
+}
+}
