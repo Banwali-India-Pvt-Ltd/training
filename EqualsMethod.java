194a94449db3665ac04bd4fcf91b55163419b5b8
Banwali-India-Pvt-Ltd <30862678+Banwali-India-Pvt-Ltd@users.noreply.github.com>
2017-09-05 09:32:17 +0530

    Add files via upload

---
+++ EqualsMethod.java
@@ -0,0 +1,16 @@
+// The program gives equal from the strings
+// class to create method
+public class EqualsMethod
+{
+	// main method to create program
+	public static void main(String[] args)
+	{
+		String str1 = " Selenium ";
+		String str2 = " UFT ";
+		String str3 = " Selenium ";
+		
+		System.out.println(str1.equals(str2));  // returns false
+		System.out.println(str1.equals(str3));  // returns true
+	}
+
+}
