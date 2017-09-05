37fa65876358d336e5f79180e9e5e5e13ea41889
Banwali-India-Pvt-Ltd <30862678+Banwali-India-Pvt-Ltd@users.noreply.github.com>
2017-09-05 10:24:23 +0530

    Add files via upload

---
+++ StringToLowercase.java
@@ -0,0 +1,17 @@
+// The program returns the lower values from the string
+// class to create method
+public class StringToLowercase
+{
+	// main method to create program
+	public static void main(String[] args)
+	{
+		String str1 = " banwali ";
+		String str2 = " BANwaLi";
+		String str3 = " BANWALI ";
+		
+		System.out.println(str1.toLowerCase());    // returns banwali
+		System.out.println(str2.toLowerCase());    // returns banwali
+		System.out.println(str3.toLowerCase());    // returns banwali
+	}
+
+}
