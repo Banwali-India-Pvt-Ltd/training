01d933566998fde020b44b35f5c301735de86901
Banwali-India-Pvt-Ltd <30862678+Banwali-India-Pvt-Ltd@users.noreply.github.com>
2017-09-05 21:44:29 +0530

    Add files via upload

--- for.1.py
+++ for.1.py
@@ -4,8 +4,8 @@
 ram_location = "jaipur"
 locations=["house","living ajamer",  "stop"]
 for i in locations:
-    if i == bus_location:
+    if i==ram_location:
         print ("ram is found in",i)
         break
     else:
-        print
+        print("ram is not found in",i)
