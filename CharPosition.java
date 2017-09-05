94a1261dee9d621c7c7c14995c6d98efb3fc3184
Banwali-India-Pvt-Ltd <30862678+Banwali-India-Pvt-Ltd@users.noreply.github.com>
2017-09-05 09:04:08 +0530

    Add files via upload

---
+++ CharPosition.java
@@ -0,0 +1,12 @@
+// The program gives the positional value
+// class to create method
+public class CharPosition
+{
+	// main method to create program
+public static void main(String[] args)
+{
+	String str1 ="SELENIUM";
+	char result = str1.charAt(3);
+	System.out.println(result); // returns E
+}
+}
