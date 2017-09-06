3713626dc21eeb9c55901a979d6a545051c98c0b
Banwali-India-Pvt-Ltd <30862678+Banwali-India-Pvt-Ltd@users.noreply.github.com>
2017-09-06 07:09:47 +0530

    Add files via upload

---
+++ TestFunction.java
@@ -0,0 +1,12 @@
+// Not actually understood that how to program performs
+public class TestFunction {
+	 public static void main(String[] args)
+	    {
+	        String str = "geeks";
+	        str.toUpperCase();
+	        str += "forgeeks";
+	        String string = str.substring(2,13);
+	        string = string + str.charAt(4);
+	        System.out.println(string);
+	    }
+}
