cb47f857ebee96ebd036e021d24e5176175ff6dd
Banwali-India-Pvt-Ltd <30862678+Banwali-India-Pvt-Ltd@users.noreply.github.com>
2017-09-06 06:35:28 +0530

    Add files via upload

---
+++ ArrayLengthMethod.java
@@ -0,0 +1,12 @@
+// The program execute the length of the array
+// class to create method
+public class ArrayLengthMethod
+{
+	// main method to create program
+	public static void main(String[] args)
+	{
+		int [] array1 = {10,20,30,40,50};
+		System.out.println(array1.length);  // 5
+	}
+
+}
