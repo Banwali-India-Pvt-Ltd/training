11d7e69a8aa8afcdcb2d21c7c252cf8aacfadc58
Banwali-India-Pvt-Ltd <30862678+Banwali-India-Pvt-Ltd@users.noreply.github.com>
2017-09-06 16:09:23 +0530

    Add files via upload

---
+++ MaxFuntion.java
@@ -0,0 +1,15 @@
+// The program returns the max numbers between the two numbers
+// class to create method
+public class MaxFuntion 
+{
+	// main method to create program
+	public static void main(String[] args)
+	{
+		
+        int n1 = 5;
+        int n2 = 50;
+        int largest = Math.max(n1, n2);
+        System.out.println(largest);
+	}
+
+}
