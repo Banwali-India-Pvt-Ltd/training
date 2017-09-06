a07264ecd39d293a217912ec4e11f3e75768ba34
Banwali-India-Pvt-Ltd <30862678+Banwali-India-Pvt-Ltd@users.noreply.github.com>
2017-09-06 08:35:29 +0530

    Add files via upload

---
+++ LengthFunction.java
@@ -0,0 +1,12 @@
+// The program returns the length of the number
+// class to create method
+public class LengthFunction
+{
+	// main method to create program
+	public static void main(String[] args)
+	{
+		int [] a = new int[0];
+		System.out.println(a.length);
+	}
+
+}
