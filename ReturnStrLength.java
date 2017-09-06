a222c3cf74eaa76f3ecf753d0d8716a3ac7df172
Banwali-India-Pvt-Ltd <30862678+Banwali-India-Pvt-Ltd@users.noreply.github.com>
2017-09-06 06:13:28 +0530

    Add files via upload

---
+++ ReturnStrLength.java
@@ -0,0 +1,12 @@
+// The program returns the length of the string 
+// class to create method 
+public class ReturnStrLength
+{
+	// main method to create program
+	public static void main(String[] args)
+	{
+		String s = "Banwali";
+		System.out.println(s.length());  // 7
+	}
+
+}
