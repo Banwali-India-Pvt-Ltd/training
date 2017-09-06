adbeaa03f1fa268b8678f3adf43ffad14e81aeb7
Banwali-India-Pvt-Ltd <30862678+Banwali-India-Pvt-Ltd@users.noreply.github.com>
2017-09-06 06:42:38 +0530

    Add files via upload

---
+++ ToStringMethod.java
@@ -0,0 +1,13 @@
+// The program returns toString 
+// import to get input
+import java.util.Arrays;
+// class to create method 
+public class ToStringMethod
+{
+	// main method to create program
+public static void main(String[] args){
+	String [] arr1 = { "UFT", "Banwali", "RFT", "Test"};
+	String str = Arrays.toString(arr1);
+	System.out.println(str);
+}
+}
