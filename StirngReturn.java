98354f7c93b031bb3541b26e54e49c571efc622f
Banwali-India-Pvt-Ltd <30862678+Banwali-India-Pvt-Ltd@users.noreply.github.com>
2017-09-06 08:03:42 +0530

    Add files via upload

---
+++ StirngReturn.java
@@ -0,0 +1,16 @@
+// The program return value only that which is in the main method
+// class to create method
+public class StirngReturn
+{
+	//  main method to create program
+	public static void main(String[] args){
+		String x = null;
+		banwali(x);
+		System.out.println(x);
+		
+	}
+	public static void banwali(String y){
+		y = " Greek ";
+		
+	}
+}
