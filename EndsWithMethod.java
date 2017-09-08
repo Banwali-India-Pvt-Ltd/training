5cb98d43959bcbd6350012b03bd25567a08b2264
Banwali-India-Pvt-Ltd <30862678+Banwali-India-Pvt-Ltd@users.noreply.github.com>
2017-09-08 11:57:20 +0530

    Add files via upload

--- EndsWithMethod.java
+++ EndsWithMethod.java
@@ -1,16 +1,18 @@
-// The program returns end value in the string
-// class to create method
+/* The program checks if the string ends with specified suffix or not
+ * And supports 2-way comparison 
+ */
+// class to create object
 public class EndsWithMethod
 {
 	// main method to create program
 	public static void main(String[] args)
 	{
-		String str1 = "Banwali Company";
+		String str = "Welcome to the Banwali Company";
 		
-		System.out.println(str1.endsWith("Company"));
-		System.out.println(str1.endsWith("any"));
-		System.out.println(str1.endsWith("Banwali"));
-		System.out.println(str1.endsWith("MEthod"));
+		System.out.println(str.endsWith("Banwali Company"));  // True
+		System.out.println(str.endsWith("Company"));          // True
+		System.out.println(str.endsWith("Banwali"));          // False
+		System.out.println(str.endsWith("Banali Company"));   // False
 	}
 
 }
