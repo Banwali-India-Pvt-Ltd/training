1bdf560dc20ab184cc96fe6d01f2365e6f4dc2a3
Banwali-India-Pvt-Ltd <30862678+Banwali-India-Pvt-Ltd@users.noreply.github.com>
2017-09-08 09:54:36 +0530

    Add files via upload

---
+++ CharAtMethod.java
@@ -0,0 +1,14 @@
+// The program returns a character value by index
+// class to create object
+public class CharAtMethod 
+{
+	// main method to create program
+	public static void main(String[] args)
+	{
+		String str1 = "Banwali";
+		
+		System.out.println(str1.charAt(1));  // a
+		System.out.println(str1.charAt(6));  // i
+	}
+
+}
