3a284b2d77e63e46806aa3533c1c9eda057489da
Banwali-India-Pvt-Ltd <30862678+Banwali-India-Pvt-Ltd@users.noreply.github.com>
2017-09-08 15:38:22 +0530

    Add files via upload

---
+++ RandomMethod.java
@@ -0,0 +1,11 @@
+// The program generates random numbers depends on compiler
+// class to create object
+public class RandomMethod
+{
+	// main method to create program
+	public static void main(String[] args)
+	{
+		System.out.println(Math.random());   // can't say what number will display
+	}
+
+}
