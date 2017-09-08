b9de1655cd0056bb62732893582d61d973797816
Banwali-India-Pvt-Ltd <30862678+Banwali-India-Pvt-Ltd@users.noreply.github.com>
2017-09-08 12:08:44 +0530

    Add files via upload

---
+++ StrLength.java
@@ -0,0 +1,15 @@
+// The program returns length of a string
+// class to create object
+public class StrLength 
+{
+	// main method to create program
+	public static void main(String[] args)
+	{
+		String str1 = "Banwali Company";
+		String str2 = "Testing";
+		
+		System.out.println(str1.length());   // 15
+		System.out.println(str2.length());   // 7
+	}
+
+}
