1ac42c70a6856b995dcca59ca0e2cfc01abef7d7
Banwali-India-Pvt-Ltd <30862678+Banwali-India-Pvt-Ltd@users.noreply.github.com>
2017-09-08 11:14:16 +0530

    Add files via upload

---
+++ TrimMethod.java
@@ -0,0 +1,16 @@
+// The program removes spaces from both sides of the string
+// class to create object
+public class TrimMethod 
+{
+	// main method to create program
+	public static void main(String[] args)
+	{
+		String str1 = "       Banwali       ";
+		String str2 = "        Company         ";
+		
+		System.out.println(str1);   
+		System.out.println(str1.trim());
+		System.out.println(str2.trim());
+	}
+
+}
