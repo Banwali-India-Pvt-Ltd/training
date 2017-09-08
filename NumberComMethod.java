d4250797b9f840e44c58fd8d73065477d4728bc0
Banwali-India-Pvt-Ltd <30862678+Banwali-India-Pvt-Ltd@users.noreply.github.com>
2017-09-08 13:55:09 +0530

    Add files via upload

---
+++ NumberComMethod.java
@@ -0,0 +1,19 @@
+// Not understood this code
+// The program compares two numbers and returns the value
+// class to create an object
+public class NumberComMethod 
+{
+	// main method to create program
+	public static void main(String[] args)
+	{
+		int x = 5;
+		Integer a = Integer.valueOf(x);
+		
+		
+		System.out.println(a.compareTo(Integer.valueOf(5)));
+		System.out.println(a.compareTo(Integer.valueOf(4)));
+		System.out.println(a.compareTo(Integer.valueOf(7)));
+		
+	}
+
+}
