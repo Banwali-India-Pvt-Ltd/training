cc4b8adffe36071e1339c093a758eeca4f15b4da
Banwali-India-Pvt-Ltd <30862678+Banwali-India-Pvt-Ltd@users.noreply.github.com>
2017-09-08 14:34:03 +0530

    Add files via upload

---
+++ AbsoluteMethod.java
@@ -0,0 +1,17 @@
+// The program returns absolute value 
+// class to create object
+public class AbsoluteMethod
+{
+	// main method to create program
+	public static void main(String[] args)
+	{
+		double a = 10.234;
+		double b = 10.789;
+		double c = -20.345;
+		
+		System.out.println(Math.abs(a));  // 10.234
+		System.out.println(Math.abs(b));  // 10.789
+		System.out.println(Math.abs(c));  // 20.345
+	}
+
+}
