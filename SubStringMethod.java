363203f945fbf03aa9a10da81682ef1a019b19bf
Banwali-India-Pvt-Ltd <30862678+Banwali-India-Pvt-Ltd@users.noreply.github.com>
2017-09-08 11:40:16 +0530

    Add files via upload

---
+++ SubStringMethod.java
@@ -0,0 +1,16 @@
+// The program returns part of the string based on index positions
+// class to create object
+public class SubStringMethod
+{
+	// main method to create program
+	public static void main(String[] args)
+	{
+		String str1 = "Welcome to Banwali";
+		
+		System.out.println(str1.substring(11));
+		System.out.println(str1.substring(7));
+		System.out.println(str1.substring(8,10));
+		System.out.println(str1.substring(2,5));
+	}
+
+}
