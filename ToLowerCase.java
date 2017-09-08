ca97126ae12d8cfd411782061d459f4c74052493
Banwali-India-Pvt-Ltd <30862678+Banwali-India-Pvt-Ltd@users.noreply.github.com>
2017-09-08 10:57:52 +0530

    Add files via upload

--- ToLowerCase.java
+++ ToLowerCase.java
@@ -1,13 +1,19 @@
-// The program converts values to upper case
-// class to create method
+// The program converts value to lower case
+// class to create object
 public class ToLowerCase 
 {
 	// main method to create program
 	public static void main(String[] args)
 	{
-		System.out.println(Character.toLowerCase('a'));  // returns a
-		System.out.println(Character.toLowerCase('A'));  // returns a
-		System.out.println(Character.toLowerCase('S'));  // returns s
+		String str1 = "Banwali";
+		String str2 = "BANWALI";
+		String str3 = "BAnWaLi";
+		String str4 = "banwali123";
+		
+		System.out.println(str1.toLowerCase());  // banwali
+		System.out.println(str2.toLowerCase());  // banwali
+		System.out.println(str3.toLowerCase());  // banwali
+		System.out.println(str4.toLowerCase());  // banwali123
 	}
 
 }
