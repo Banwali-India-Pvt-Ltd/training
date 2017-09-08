573c8da3624d2e104882e16176c1896cbd743762
Banwali-India-Pvt-Ltd <30862678+Banwali-India-Pvt-Ltd@users.noreply.github.com>
2017-09-08 09:20:25 +0530

    Add files via upload

---
+++ EqualsToMethod.java
@@ -0,0 +1,16 @@
+// The program checks weather the value is equal to or not
+// class to create object
+public class EqualsToMethod 
+{
+	// main method to create program
+	public static void main(String[] args)
+	{
+		String str1 = "banwali";
+		String str2 = "BANWALI";
+		String str3 = "banwali";
+		
+		System.out.println(str1.equals(str2));  // False
+		System.out.println(str1.equals(str3));  // True
+	}
+
+}
