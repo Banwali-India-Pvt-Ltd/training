4e047a263016f60d321cf7b38cbef3de7771b768
Banwali-India-Pvt-Ltd <30862678+Banwali-India-Pvt-Ltd@users.noreply.github.com>
2017-09-09 15:50:59 +0530

    Add files via upload

---
+++ MinWithReturnvalue.java
@@ -0,0 +1,28 @@
+// The program execute minimum value between two numbers with return
+// class to create an object
+public class MinWithReturnvalue
+{
+	// main method to create program
+	public static void main(String[] args) {
+	      int a = 11;
+	      int b = 6;
+	      int c = minFunction(a, b);
+	      System.out.println("Minimum Value = " + c);
+	   }
+
+	   /** returns the minimum of two numbers */
+	   public static int minFunction(int n1, int n2) 
+	   {
+	      int min;
+	      if (n1 > n2)
+	      { 	  
+	         min = n2;
+	      }
+	      else
+	      {
+	         min = n1;
+	      }
+	      
+	      return min;
+	   }
+}
