e0ace095b9f38a1af12ebeb0a4b85b52badd1489
Banwali-India-Pvt-Ltd <30862678+Banwali-India-Pvt-Ltd@users.noreply.github.com>
2017-09-09 09:01:20 +0530

    Add files via upload

---
+++ LengthArrayMethod.java
@@ -0,0 +1,13 @@
+// The program returns length of an array
+// class to create an object
+public class LengthArrayMethod
+{
+	// main method to create program
+	public static void main(String[] args)
+	{
+		int [] array = { 10,20,30,40,50 };
+		
+		System.out.println(array.length);     // 5
+	}
+
+}
