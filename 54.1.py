75db7f5d2a6b20ebf6ab80f3af6971296abfc284
Banwali-India-Pvt-Ltd <30862678+Banwali-India-Pvt-Ltd@users.noreply.github.com>
2017-09-13 16:49:56 +0530

    Add files via upload

---
+++ 54.1.py
@@ -0,0 +1,22 @@
+# The program defines values assign them and print
+
+i1 = 2   # store value 2 in i1
+i2 = 5   # store value 5 in i2
+i3 = -3   # store value -3 in i3
+d1 = 2.0   # store value 2.0 in d1
+d2 = 5.0    # store value 5.0 in d2
+d3 = -0.5    # store value -0.5 in d3
+
+
+print(i1 + (i2 * i3))  # print multiple i2 & i3 and add i1
+print(i1 * (i2 + i3))  # print add i2 & i3 and multiple i1
+print(i1 / (i2 + i3))  # print add i2 & i3 and divide into i1
+print(i1 // (i2 + i3)) # could't assign // signs
+print(i1 / i2 + i3)    # print i2 & i3 and divide into i1
+print(i1//i2 + i3)     # couldn't assign signs
+print(3 + 4 + 5 / 3)   # print after calculate equation
+print(3 + 4 + 5 // 3)  # couldn't assign // sign
+print(3 + 4 + 5) // 3  # print after calculate equation
+print(3 + 4 + 5) // 3  # print after calculate equation
+print(d1 + (d2 * d3))  # print multiply d2 & d3 and add d1
+print(d1 + d2 * d3)    # print multiply d2 & d3 and d1
