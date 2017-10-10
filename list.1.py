e414593ba18606869cdf40f2e32209a8db82c221
Banwali-India-Pvt-Ltd <30862678+Banwali-India-Pvt-Ltd@users.noreply.github.com>
2017-10-10 16:48:59 +0530

    Add files via upload

---
+++ list.1.py
@@ -0,0 +1,9 @@
+# list is a data structure
+p = ['abc', 'def', 'fgi', 'sdfsd', 'sdfs']
+q = [1, 2, 3, 4, 5]
+
+i = 0 # initialize
+while i < 5:
+    print p[i], q[i] # indexing the ith element
+    i = i + 1 # incresing the whle-loop counter= iiC:\Python27\python.exe "C:/Users/Banwali RS-CIT/list
+    
---
+++ list.py
@@ -0,0 +1,4 @@
+# list is a container data structure
+# container: used to stpre objects
+p = [1, 2, 'apple', 'orange']
+print p[0:4]# indexing starts at 0 and does not include the last number in
