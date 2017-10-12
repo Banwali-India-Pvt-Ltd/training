ec23d6d3d29d589b85df20e5efcfe8956e36c9bc
Banwali-India-Pvt-Ltd <30862678+Banwali-India-Pvt-Ltd@users.noreply.github.com>
2017-10-12 16:57:51 +0530

    Add files via upload

---
+++ list.2.py
@@ -0,0 +1,4 @@
+# list: changing the contents
+p = [1, 2, 'apple','orange']
+p[2] = 10
+print p
---
+++ list.3.py
@@ -0,0 +1,4 @@
+#  list
+"11 = [1, 2, 3]# list"
+"13 = [4] # a list with only one element"
+print 11+13
