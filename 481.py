468fedfc22093111ce374d13010ceba9c9dca5c6
Banwali-India-Pvt-Ltd <30862678+Banwali-India-Pvt-Ltd@users.noreply.github.com>
2017-10-23 18:28:01 +0530

    Add files via upload

---
+++ 481.py
@@ -0,0 +1,7 @@
+population_dict = {'California': 38332521,
+							'Texas': 26448193,
+							'New York': 19651127,
+							'Florida': 19552860,
+							'Illinois': 12882135}
+population = pd.Series(population_dict)
+population
---
+++ 482.py
@@ -0,0 +1,3 @@
+area_dict = {'California': 423967, 'Texas': 695662, 'New York': 141297, 'Florida': 170312, 'Illinois': 149995}
+print area_dict
+
