d440f075765dca62609f32fad749b64aef68668d
Banwali-India-Pvt-Ltd <30862678+Banwali-India-Pvt-Ltd@users.noreply.github.com>
2017-10-31 19:37:50 +0530

    Add files via upload

---
+++ 534.py
@@ -0,0 +1,7 @@
+words = {}
+words["hello"] = "bonjour"
+words["yes"] = "oui"
+words["no"]  = "Non"
+words["bye"] = "au revoir"
+print (words["hello"])
+print (words["no"])
---
+++ 535.py
@@ -0,0 +1,7 @@
+dict = {}
+dict['ford'] = "car"
+dict['python'] = "the python progn"
+dict[2] = "this sentence is storec"
+print (dict['ford'])
+print (dict['python'])
+print (dict[2])
