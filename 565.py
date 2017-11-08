33eef9b0c3f3d2ea7c4b27aadb100dcbbe27d988
Banwali-India-Pvt-Ltd <30862678+Banwali-India-Pvt-Ltd@users.noreply.github.com>
2017-11-08 22:02:40 +0530

    Add files via upload

--- 565.py
+++ 565.py
@@ -1,4 +1,5 @@
 from  __future__ import  print_function
+n = int(input("please enter the number,=>"))
 j = 9
 for i in range(1,10,2):
     print(' '*j+i*"*",)
---
+++ 570.py
@@ -0,0 +1,6 @@
+from  __future__ import  print_function
+num = int(input("enter the number of rows:"))
+for i in range(num,0,-1):
+    for j in range(0,i):
+        print ("*",end="")
+    print()
